63b9c3595267a675d9989ea80b3faa9b1c31c00d
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2019-08-03 13:13:15 +0200

    Minor cleanup and 2to3

--- scripts/copyphotos.py
+++ scripts/copyphotos.py
@@ -8,17 +8,17 @@ dirs = os.listdir(base)
 target = []
 
 for name in dirs:
-	if 'Ampar' in name:
-		#print name
-		target.append(name)
-		
+  if 'Ampar' in name:
+    # print name
+    target.append(name)
+
 for name in target:
-	src = os.path.join(base, name)
-	dest = os.path.join('E:', name)
-	print name, '[', len( os.listdir(src) ), ']'
-	try:
-		shutil.copytree(src, dest)
-	except shutil.Error as e:
-		print e
-	except shutil.OSError as e:
-		print e
+  src = os.path.join(base, name)
+  dest = os.path.join('E:', name)
+  print(name, '[', len(os.listdir(src)), ']')
+  try:
+    shutil.copytree(src, dest)
+  except shutil.Error as e:
+    print(e)
+  except shutil.OSError as e:
+    print(e)
