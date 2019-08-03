470ad0867b07c8cc311f4fe92baf0b248b74ff8e
Pedro Crespo-Valero <pedro.crespovalero@gmail.com>
2019-08-03 15:06:58 +0200

    script to delete empty folders ready

--- scripts/delete_empty_folders.py
+++ scripts/delete_empty_folders.py
@@ -2,25 +2,29 @@
 Borra directorios sin fotos
 '''
 import os
+import sys
 
 from send2trash import send2trash
 
-
-def delemptydirs(top_path):
+def delemptydirs(top_path, *, dry_run=False):
   del_dirs = []
   for root, dirs, files in os.walk(top_path):
     if len(dirs) == 0 and len(files) <= 1:
-      if len(files) == 0 or (len(files) > 0 and files[0] == ".picasa.ini"):
+      if not files or files == [".picasa.ini"]:
         del_dirs.append(root)
+
+    dirs[:] = [dirname for dirname in dirs if not dirname.startswith(".")]
 
   for dir in del_dirs:
     print("deleting", dir, "...")
-    # send2trash(dir)
-
-  for dir in dirs:
-    if dir.startswith("."):
-      dirs.remove(dir)
-
+    if not dry_run:
+      send2trash(dir)
 
 if __name__ == "__main__":
-  delemptydirs(r"D:\Usuarios\papacrespo\Pictures")
+  if len(sys.argv)==1:
+    print(f"delete_empty_folders [--dry-run] path/to/top/folder")
+    exit(1)
+  
+  top = sys.argv[-1]
+  ops = sys.argv[1:-1]
+  delemptydirs(top, dry_run="--dry-run" in ops)
